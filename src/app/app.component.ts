4877e321eed11e5402468129df734afad6f5ae98
FullStackDeveloper-j <128317139+FullStackDeveloper-j@users.noreply.github.com>
2023-05-02 19:17:20 +0530

    done the read operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//importing the httpClient
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CrudOperation';
+  // reading from the json of person
+  url = "http://localhost:3000/person"
+  data1:any
+
+  //creating an constructor to read the data to store it in html
+  constructor(private http:HttpClient)
+  {
+    //using http and getting url specified in the App.componet.url
+    // subscribing it and using the arrow function to store the data
+    http.get(this.url).subscribe(datafromjson=>{
+      //storing it in data1 
+      this.data1 = datafromjson
+    })
+
+  }
+
+
 }
