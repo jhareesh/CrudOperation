69010c27164e55d79741e0a4b785b569692b33f4
FullStackDeveloper-j <128317139+FullStackDeveloper-j@users.noreply.github.com>
2023-05-03 18:10:29 +0530

    read update delete operations added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,17 @@ export class AppComponent {
   //variable to store the data from html below
   data1:any;
 
-  //defining variable
+  //defining variable of creation form ngModel textBoxes
   nametemplatecreate : any;
   phonetemplatecreate : any;
   emailtemplatecreate : any;
+
+  //defining variable for modification form ngModel textBoxes 
+  nametemplateModify : any;
+  phonetemplateModify : any;
+  emailtemplateModify : any;
+  tsId : any //to store the id of data
+  index : any //to store the index of data
 
   //creating an constructor to read the data to store it in html
   constructor(private http:HttpClient)
@@ -34,11 +41,42 @@ export class AppComponent {
   //defining function
   onSubmitCreate(dataCreate:any)
   {
-      //using http and getting url specified in the App.componet.url
+    //using http and getting url specified in the App.componet.url
     // subscribing it and using the arrow function to store the data and pushed simultaneously
     this.http.post(this.url,dataCreate.value).subscribe(res=>{
       this.data1.push(res)
     })
   }
 
+  //for deletion 
+  onDelete(deletedata:any)
+  {
+    //using http and getting url specified in the App.componet.url
+    // subscribing it and using the arrow function to delete the data and remove from json server simultaneously
+    this.http.delete(this.url+"/"+deletedata.id).subscribe(()=>{
+      //performing deletion on one element by using index
+      this.data1.splice(this.data1.indexOf(deletedata),1)
+    })
+  }
+
+  //defining method for update
+  onUpdate(element:any)
+  {
+    //sending the data to the textboxes by using storing the elements data in the ngModel textBoxes Variables
+    this.nametemplateModify = element.namejson
+    this.emailtemplateModify = element.emailjson
+    this.phonetemplateModify = element.phonejson
+    
+    //storing id and index of the element
+    this.tsId = element.id
+    this.index = this.data1.indexOf(element)
+  } 
+
+  //definig method for modify
+  onModify(dataModify : any)
+  {
+    this.http.put(this.url+"/"+this.tsId,dataModify.value).subscribe(()=>{
+      this.data1[this.index] = dataModify.value
+    })
+  }
 }
